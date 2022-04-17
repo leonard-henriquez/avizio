5aa48b2069a7d56280c07652356d22e5c1302247
Léonard Henriquez <leonard.henriquez@edhec.com>
2022-04-17 11:30:55 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const withTM = require('next-transpile-modules')([
+  "@fullcalendar/common",
+  "@babel/preset-react",
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/timegrid",
+  "@fullcalendar/interaction",
+  "@fullcalendar/react",
+])
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig)
